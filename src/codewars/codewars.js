306c3b0081d5a55702581c6a4b0ca6d2aea64e83
nargiz92 <ada.tag.92@mail.ru>
2023-06-10 21:33:27 +0300

    Trolls are attacking your comment section!

--- src/codewars/codewars.js
+++ src/codewars/codewars.js
@@ -1,6 +1,60 @@
 
 
-const countSheeps = (arrOfSheep) => {
-    return arrOfSheep.filter(el=>el===true).length
+//  const countSheeps = (arrOfSheep) => {
+//     return arrOfSheep.filter(el=>el===true).length
+// }
+// console.log(countSheeps([true,true,false]))
+
+
+// Abbreviate a Two Word Name;
+//  Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+//
+//      The output should be two capital letters with a dot separating them.
+//
+//      It should look like this:
+//
+//  Sam Harris => S.H
+//
+//  patrick feeney => P.F
+
+// Solution
+//
+//  function abbrevName(name){
+//     return  name.map(word => `${word[0].toUpperCase()}.`).join('')
+//
+//  }
+//
+//  console.log(abbrevName("Sam Harris"))
+
+//--------------------------------------------------------------------------
+// Trolls are attacking your comment section!
+//
+//     A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+//
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+//
+//     For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+//
+//     Note: for this kata y isn't considered a vowel.
+
+//Solution
+function disemvowel(str) {
+    return str.replace(/[aeuio]/gi,'')
 }
-console.log(countSheeps([true,true,false]))
+
+console.log(disemvowel('hOAEjdeeareaoiyu'))
+//-------------------------------------------------------------------------------------------------------
+// Hello World - Without Strings
+// You need to create a function, helloWorld, that will return the String Hello, World!
+// without actually using raw strings. This includes quotes, double quotes and template strings.
+// You can, however, use the String constructor and any related functions.
+//
+
+//solution
+// const helloWorld = () => {
+// return ${Hello, World!.replace(/['"`]/g,'');
+// // const greetings=['Hello, World!']
+// // return greetings.toString()
+// };
+//
+// console.log(helloWorld())
