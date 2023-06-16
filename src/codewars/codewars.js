987dc5a5366f7affbde0435cd7432de747dd2c80
nargiz92 <ada.tag.92@mail.ru>
2023-06-16 10:22:52 +0300

    A Needle in the Haystack

--- src/codewars/codewars.js
+++ src/codewars/codewars.js
@@ -178,3 +178,24 @@ function greet1(name){
 }
 
 console.log(greet1('Nargiza'))
+//---------------------------------------------------------------------------------------------------------
+//A Needle in the Haystack
+//Can you find the needle in the haystack?
+//
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//
+// After your function finds the needle it should return a message (as a string) that says:
+//
+// "found the needle at position " plus the index it found the needle
+
+//Solution:
+
+function findNeedle(haystack) {
+  const stack=haystack.findIndex((st)=>{
+      return st==='needle'
+  })
+    return `found the needle at position ${stack}`
+    // your code here
+}
+
+console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
