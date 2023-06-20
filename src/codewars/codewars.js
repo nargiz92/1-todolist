4f4409895da3f91c4d5167bd8e5bb3275eea22d6
nargiz92 <ada.tag.92@mail.ru>
2023-06-20 13:31:55 +0300

    Are You Playing Banjo?

--- src/codewars/codewars.js
+++ src/codewars/codewars.js
@@ -221,3 +221,26 @@ function findNeedle(haystack) {
 function maps(x){
     return x.map(el=>el+el)
 }
+//-------------------------------------------------------------------------------------------------------
+//Convert a Boolean to a String
+//Implement a function which convert the given boolean value into its string representation.
+//Solution
+const booleanToString=(b)=> b===true?'true':'false'
+//------------------------------------------------------------------------------------------------------------------
+//Are You Playing Banjo?
+//Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+//
+// The function takes a name as its only argument, and returns one of the following strings:
+//
+// name + " plays banjo"
+// name + " does not play banjo"
+
+//Solution
+
+function areYouPlayingBanjo(name) {
+    // Implement me
+    return name[0]==='R'?`${name} plays banjo`:(name[0]==='r')?`${name} plays banjo`:`${name} does not play banjo`
+}
+
+console.log(areYouPlayingBanjo('ramira'))
