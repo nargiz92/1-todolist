df297d59bd8bbefbbc8ad563986d38a87822bd15
nargiz92 <ada.tag.92@mail.ru>
2023-07-06 22:56:00 +0300

    You only need one - Beginner

--- src/codewars/codewars.js
+++ src/codewars/codewars.js
@@ -286,3 +286,18 @@ const makeUpperCase=(str) =>str.toUpperCase()
     // Code here
 
 console.log(makeUpperCase('hello'))
+//__________________________________________________________________________________________________---
+//You only need one - Beginner
+//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+//
+// Array can contain numbers or strings. X can be either.
+//
+// Return true if the array contains the value, false if not.
+//Solution
+
+function check(a, x) {
+return a.includes(x)
+}
+
+console.log(check([5,10],5))
+console.log(check([5,10],1))
